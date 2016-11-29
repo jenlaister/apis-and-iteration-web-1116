28018abc174e3dcfaeba872e4b67e4edc1b1464d
Jenny Lai <jenny.lai@flatironschool.com>
2016-11-29 16:23:17 -0500

    done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,11 +2,31 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def get_character_hash
+  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  JSON.parse(all_characters)
+end
+
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
+  films_array = []
+  #return_array = []
+  #character_hash = get_character_hash
+    get_character_hash["results"].each { |character_h|
+      if character == character_h["name"].downcase
+      films_array = character_h["films"]
+    end
+    }
+    #return_array = films_array.collect { |film_api|
+    films_array.collect { |film_api|
+      #film_info = RestClient.get(film_api)
+      film_hash = JSON.parse(film_info = RestClient.get(film_api))
+      #return_array.push(film_hash)
+    }
+  #   if return_array.length == 0
+  #     puts "Character is not found in database."
+  # end
+  #return_array
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -20,6 +40,9 @@ end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each.with_index(1) { |film, index|
+    puts "#{index}. #{film["title"]}"
+  }
 end
 
 def show_character_movies(character)
